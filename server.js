687f29e270b359082a766579d5ba43a23c4d5818
Lucas <lhelme123@gmail.com>
2019-02-24 08:05:54 -0500

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
-const app = express();
-process.env.PORT
+const PORT = process.env.PORT || 3000;
 
 // Set public folder as root
 app.use(express.static('public'));
